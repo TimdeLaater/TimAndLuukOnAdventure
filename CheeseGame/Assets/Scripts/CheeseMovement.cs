4cae9cff1051348cd4c266ae56ac539013bf6569
TimdeLaater <tdelaater@gmail.com>
2023-06-06 14:30:25 +0200

    Fixed movement

--- CheeseGame/Assets/Scripts/CheeseMovement.cs
+++ CheeseGame/Assets/Scripts/CheeseMovement.cs
@@ -6,45 +6,24 @@ public class CheeseMovement : MonoBehaviour
     public float speed; // Speed of the cheese
     public float rollSpeedThreshold; // Minimum speed required to prevent the cheese from falling
     public float rollingResistance; // Resistance to slow down the roll
-    public float throwForceScale = 0.2f; // Scaling factor for throw force
-    public float maxVerticalThrowForce = 5f; // Maximum value for the throw force in the vertical direction
+    public float horizontalTorqueScale = 0.5f; // Scaling factor for horizontal torque
+    public float maxVelocity = 10f; // Maximum velocity limit
     public BoxCollider boxCollider; // Reference to the BoxCollider component
 
-    private Vector3 mouseDownPosition; // Mouse position when mouse button is pressed down
-    private bool isMousePressed; // Flag to track if the mouse button is pressed
+   
     private bool hasThrown; // Flag to track if the cheese has been thrown
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        isMousePressed = false;
         hasThrown = false;
     }
-
-    private void Update()
+    private void OnTriggerStay(Collider other)
     {
-        if (!hasThrown && Input.GetMouseButtonDown(0))
-        {
-            // Store the mouse position when the mouse button is pressed down
-            mouseDownPosition = Input.mousePosition;
-            isMousePressed = true;
-        }
-        else if (isMousePressed && Input.GetMouseButtonUp(0))
-        {
-            // Calculate the difference between the current mouse position and the mouse position when the button was pressed down
-            Vector3 mouseUpPosition = Input.mousePosition;
-            Vector3 throwForce = (mouseUpPosition - mouseDownPosition) * throwForceScale;
-
-            // Limit the vertical throw force
-            throwForce.y = Mathf.Clamp(throwForce.y, -maxVerticalThrowForce, maxVerticalThrowForce);
-
-            // Apply the throw force to the cheese
-            rb.AddForce(throwForce, ForceMode.Impulse);
-
-            isMousePressed = false;
-            hasThrown = true;
-        }
+            
     }
+
+    
 
     private void FixedUpdate()
     {
@@ -61,12 +40,17 @@ public class CheeseMovement : MonoBehaviour
             if (boxCollider.bounds.Contains(transform.position))
             {
                 rb.AddForce(movement * speed, ForceMode.Force);
+
+                rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity);
             }
 
             // Apply rolling resistance to slow down the roll
             Vector3 angularVelocity = rb.angularVelocity;
+
+            // Reduce the effect of horizontal movement on angular velocity
+            Vector3 horizontalTorque = Vector3.up * horizontal * horizontalTorqueScale;
             Vector3 resistanceTorque = -angularVelocity.normalized * rollingResistance;
-            rb.AddTorque(resistanceTorque);
+            rb.AddTorque(resistanceTorque + horizontalTorque);
 
             // Check if the roll speed falls below the threshold
             if (rb.angularVelocity.magnitude < rollSpeedThreshold)
